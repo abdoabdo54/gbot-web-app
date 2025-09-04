0f6256c7034519a7df8aa0a065441916e7e7dd65
wieckdavid4-alt <wieckdavid4@gmail.com>
2025-09-04 14:57:18 +0100

    Add missing files

--- app.py
+++ app.py
@@ -2281,4 +2281,4 @@ def add_from_server_json():
         return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
